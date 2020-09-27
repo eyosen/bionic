04d70db52cdf74240cb24cf9ee480ceb51acb70b
SpiritCroc <dev@spiritcroc.de>
2020-09-27 11:48:14 +0300

    Make hosts cache respect toggle-able adblock file
    Restore behaviour of
    8cfc70d netd: Toggle-able adblock hosts file [1/3]
    after
    466ac698 netd: Sort and cache hosts file data for fast lookup
    
    Change-Id: I23e41b2fffa9415025ca9a2c7afcdea31d1b2461

--- libc/dns/net/hosts_cache.c
+++ libc/dns/net/hosts_cache.c
@@ -36,6 +36,8 @@
 #include "hostent.h"
 #include "resolv_private.h"
 
+#include "gethostsfile.h"
+
 #define MAX_ADDRLEN	(INET6_ADDRSTRLEN - (1 + 5))
 #define MAX_HOSTLEN	MAXHOSTNAMELEN
 
@@ -66,6 +68,7 @@ struct hcfile
 	int		h_fd;
 	struct stat	h_st;
 	char		*h_data;
+	const char      *h_path;
 
 	uint32_t	c_alloc;
 	uint32_t	c_len;
@@ -218,8 +221,10 @@ static int _hcfilemmap(void)
 	char *h_addr;
 	const char *p, *pend;
 	uint32_t c_alloc;
+	const char *h_path;
 
-	h_fd = open(_PATH_HOSTS, O_RDONLY);
+	h_path = gethostsfile();
+	h_fd = open(h_path, O_RDONLY);
 	if (h_fd < 0)
 		return -1;
 	if (flock(h_fd, LOCK_EX) != 0) {
@@ -230,7 +235,8 @@ static int _hcfilemmap(void)
 	if (hcfile.h_data) {
 		memset(&st, 0, sizeof(st));
 		if (fstat(h_fd, &st) == 0) {
-			if (st.st_size == hcfile.h_st.st_size &&
+			if (strcmp(h_path, hcfile.h_path) == 0 &&
+			    st.st_size == hcfile.h_st.st_size &&
 			    st.st_mtime == hcfile.h_st.st_mtime) {
 				flock(h_fd, LOCK_UN);
 				close(h_fd);
@@ -258,6 +264,7 @@ static int _hcfilemmap(void)
 	hcfile.h_fd = h_fd;
 	hcfile.h_st = st;
 	hcfile.h_data = h_addr;
+	hcfile.h_path = h_path;
 
 	c_alloc = 0;
 	/*
